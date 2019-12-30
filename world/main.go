dd4abab3959d6c0e67b7f276151eff89c7546840
krymtkts <takatoshi.kuriyama@gmail.com>
2019-12-30 23:51:58 +0900

    Add sending message to Slack using incoming webhook.

--- world/main.go
+++ world/main.go
@@ -8,6 +8,8 @@ import (
 	"math"
 	"math/big"
 	"math/rand"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,6 +21,7 @@ import (
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
+var requestURI = os.Getenv("incoming_webhook_uri")
 var mccallVoices = []string{
 	"力になりたくても、やる気がなければ無理だ。",
 	"完璧さよりも進歩だ。",
@@ -26,13 +29,13 @@ var mccallVoices = []string{
 	"直感だよ。",
 	"老人は老人で魚は魚だ。自分以外のものにはなれない、何があっても。",
 	"自分を疑えば失敗する。",
-	"なれるよ、そうなりたいと、望むなら、何にでもなれる。",
+	"なれるよ。そうなりたいと、望むなら、何にでもなれる。",
 	"世界を変えろ。",
 	"ああ、ちょっとドジを踏んでね。",
 	"ベストを尽くせ。",
 	"警官は倫理の象徴だ、クズが。保護と奉仕、法の遵守、正義、忘れたか。",
 	"ある日誰かがひどいことをする。被害者とは他人だが、見過ごせない。なぜなら、力になってやれるからだ。",
-	"正義を行うんだ、正義を行え、正義を行え、いい警官たちのために。",
+	"正義を行うんだ。正義を行え。正義を行え。いい警官たちのために。",
 	"組織とビジネスは潰す、ひとつずつ、一ドルずつ、一人ずつ。",
 	"雨乞いをするならぬかるみも覚悟しろ。",
 	"さっきその目に何が見えるか聞いたな？私の目には何が見える。",
@@ -49,22 +52,34 @@ func getMccallVoice(index int) string {
 	return mccallVoices[index]
 }
 
+func sendToSlack(message string) error {
+	var buf bytes.Buffer
+	body, err := json.Marshal(map[string]interface{}{
+		"text": message,
+	})
+	json.HTMLEscape(&buf, body)
+
+	req, err := http.NewRequest(http.MethodPost, requestURI, &buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return err
+}
+
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
-
-	body, err := json.Marshal(map[string]interface{}{
-		"message": getMccallVoice(getRandomIndex()),
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
-	}
-	json.HTMLEscape(&buf, body)
+	var message = getMccallVoice(getRandomIndex())
+	sendToSlack(message)
 
 	resp := Response{
 		StatusCode:      200,
 		IsBase64Encoded: false,
-		Body:            buf.String(),
 		Headers: map[string]string{
 			"Content-Type":           "application/json",
 			"X-MyCompany-Func-Reply": "world-handler",
